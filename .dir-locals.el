4304ff48b19a56cb6729ce81b2f737b06a7bcb1a
Nikita Rukin <n.rukin@gmail.com>
2023-06-02 16:15:45 +0300

    Русский язык по-умолчанию

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((user-mail-address . "nikita-rukin@yandex.ru")
+((org-mode . ((org-export-default-language . "ru")
+	      (user-mail-address . "nikita-rukin@yandex.ru")
 	      (user-full-name . "Никита Рукин"))))
